9ebaacf58199fefa3b7d3bf1c9c88752882f59df
Eupedromiguel <pedromiguelataides@gmail.com>
2026-01-22 18:54:03 -0300

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -58,8 +58,6 @@ const About = ({ isDark }) => {
                 Pronto para dar vida às suas ideias?
               </h4>
               <p className="opacity-80 mb-4 md:mb-6 text-sm md:text-base">
-                Posso impulsionar seu projeto ou fazer parte da sua equipe.
-                <br />
                 Email : pedromiguelataides@gmail.com
               </p>
 
